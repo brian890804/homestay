a5cde781e22316b1d4a476995d3819f96815f48a
brian890804 <71435901+brian890804@users.noreply.github.com>
2022-11-02 17:49:36 +0800

    feat:新增 選像圖片、檔案 && 彈出民宿介紹調整

--- src/app/module/components/RoomIntroduce/FloatIntroduce.jsx
+++ src/app/module/components/RoomIntroduce/FloatIntroduce.jsx
@@ -1,3 +1,4 @@
+import { Grid } from "@mui/material";
 import styled from "@emotion/styled";
 import { Scrollbar } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
@@ -5,33 +6,50 @@ import "swiper/css";
 import "swiper/css/scrollbar";
 import "./swiper.css";
 
+import ZoomInMapIcon from "@mui/icons-material/ZoomInMap";
 import useResize from "../../Event/Resize";
+import { options } from "./Options";
 
 export default function FloatIntroduce({ introduce, handleClick }) {
   const { isMobile } = useResize();
   const { imgItems, text, option, title } = introduce;
   return (
     <FloatIntroduceElement isMobile={isMobile}>
-      <div className="container">
+      <div className="float_container">
         <Swiper
           scrollbar={{
             hide: true,
           }}
           modules={[Scrollbar]}
-          className="mySwiper"
+          className="mySwiper wiper mb-3"
         >
           {imgItems.map((img, index) => (
-            <SwiperSlide>
-              <img src={img} alt={`${index} img`} key={index} />
+            <SwiperSlide className="wiper" key={index}>
+              <img src={img} alt={`${index} img`} />
             </SwiperSlide>
           ))}
         </Swiper>
-        <div>{title}</div>
-        <div>{option}</div>
-        <div>{text}</div>
+        <div className="float_container_content">
+          <div className="float_container_title">{title}</div>
+          <div className="float_container_option">
+            <Grid container direction="row" alignItems="center" spacing={2}>
+              {option.map((data) => {
+                const { text, icon } = options[data];
+                return (
+                  <Grid item key={text}>
+                    <img src={icon} alt={text} />
+                    <label>{text}</label>
+                  </Grid>
+                );
+              })}
+            </Grid>
+          </div>
+          <div className="divider my-3"> &nbsp;</div>
+          <div className="float_container_text">{text}</div>
+        </div>
       </div>
       <div className="close cursor" onClick={handleClick}>
-        X
+        <ZoomInMapIcon fontSize="large" />
       </div>
     </FloatIntroduceElement>
   );
@@ -39,6 +57,7 @@ export default function FloatIntroduce({ introduce, handleClick }) {
 
 const FloatIntroduceElement = styled.div`
   /*  */
+  padding: 5%;
   position: fixed;
   top: 0;
   left: 0;
@@ -57,43 +76,91 @@ const FloatIntroduceElement = styled.div`
       opacity: 1;
     }
   }
-  animation:fadeIn 0.5s ;
+  animation: fadeIn 0.5s;
 
   li {
     list-style: none;
   }
 
-  img {
-    width: 100%;
-    object-fit: contain;
- 
+  ol {
+    padding: 0;
+  }
+
+  .wiper {
+    height: auto;
+    min-height: 50vh;
     @keyframes move {
       0% {
-        transform:translate(0,0)
+        transform: translate(0, 0);
       }
-      25%{
-        transform:translate(-20px,0)
+      25% {
+        transform: translate(-40px, 0);
       }
       50% {
-        transform:translate(20px,0)
+        transform: translate(0px, 0);
       }
       75% {
-        transform:translate(0,0)
+        transform: translate(-60px, 0);
       }
     }
-    animation:move 2s 2s;
+    animation: move 2s 1s;
+    @media (min-width: 599px) {
+      max-width: 40vw;
+    }
   }
-  .container {
-    padding: 5%;
-    display: flex;
-    flex-flow: column;
-    align-items: center;
-    justify-content: center;
+  .divider {
+    background-color: black;
+    height: 2px;
+    width: 100%;
+    @media (min-width: 599px) {
+      width: 40vw;
+    }
   }
+  .float {
+    &_container {
+      display: flex;
+      flex-flow: column;
+      align-items: center;
+      justify-content: center;
+      height: 100%;
+
+      &_content {
+        display: flex;
+        flex-flow: column;
+        justify-content: center;
+        width: 100%;
+        @media (min-width: 599px) {
+          width: auto;
+        }
+      }
+
+      &_title {
+        font-size: 1.4rem;
+        @media (min-width: 599px) {
+          font-size: 1.5rem;
+        }
+      }
+
+      &_option {
+        font-size: 0.6rem;
+        @media (min-width: 599px) {
+          font-size: 1rem;
+        }
+      }
+
+      &_text {
+        @media (min-width: 599px) {
+          font-size: 1.3rem;
+        }
+      }
+    }
+  }
+
   .close {
     position: absolute;
-    top: 5%;
+    top: 0%;
     right: 5%;
+    z-index: 999;
     font-size: 2rem;
     font-weight: 800;
   }
---
+++ src/app/module/components/RoomIntroduce/Options.jsx
@@ -0,0 +1,32 @@
+import sofa from "../../../../assets/Room/option/sofa.png";
+import air_conditioner from "../../../../assets/Room/option/air_conditioner.png";
+import shoe_dryer from "../../../../assets/Room/option/shoe_dryer.png";
+import bed from "../../../../assets/Room/option/bed.png";
+import hair_dryer from "../../../../assets/Room/option/hair_dryer.png";
+import hanger from "../../../../assets/Room/option/hanger.png";
+import kettle from "../../../../assets/Room/option/kettle.png";
+import magnetic_card from "../../../../assets/Room/option/magnetic_card.png";
+import no_smoke from "../../../../assets/Room/option/no_smoke.png";
+import slippers from "../../../../assets/Room/option/slippers.png";
+import television from "../../../../assets/Room/option/television.png";
+import terrace from "../../../../assets/Room/option/terrace.png";
+import towel from "../../../../assets/Room/option/towel.png";
+import tub from "../../../../assets/Room/option/tub.png";
+import wifi from "../../../../assets/Room/option/wifi.png";
+export const options = {
+  0: { icon: sofa, text: "沙發" },
+  1: { icon: tub, text: "浴缸" },
+  2: { icon: terrace, text: "露臺" },
+  3: { icon: towel, text: "浴巾" },
+  4: { icon: air_conditioner, text: "空調" },
+  5: { icon: television, text: "電視" },
+  6: { icon: wifi, text: "免費WiFi" },
+  7: { icon: no_smoke, text: "禁菸" },
+  8: { icon: bed, text: "加大雙人床" },
+  9: { icon: kettle, text: "熱水壺" },
+  10: { icon: hanger, text: "衣架" },
+  11: { icon: slippers, text: "室內拖鞋" },
+  12: { icon: hair_dryer, text: "吹風機" },
+  13: { icon: magnetic_card, text: "客房感應卡" },
+  14: { icon: shoe_dryer, text: "烘鞋機" },
+};
