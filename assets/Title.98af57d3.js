a1c885827c813f0f46eb9e6a239a58dcb17f138a
brian890804 <71435901+brian890804@users.noreply.github.com>
2022-11-17 16:29:35 +0800

    Updates

---
+++ assets/Title.98af57d3.js
@@ -0,0 +1,1 @@
+import{u as s}from"./Routes.8cb01e11.js";import{a as o}from"./index.135807fd.js";function a({children:t,id:i}){const{isMobile:e}=s();return o("div",{id:i,className:"g-bc g-center text-white fw-bold ",style:{height:e?40:70,fontSize:e?"1rem":"2rem"},children:t})}export{a as T};
