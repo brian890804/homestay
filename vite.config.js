356614a38aacedf41ade543396b55c52cc274120
brian890804 <71435901+brian890804@users.noreply.github.com>
2022-11-17 15:48:59 +0800

    更改網址測試

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'https://brian890804.github.io/homestay/',
+  base:'https://brian890804.github.io/homestayaaa/',
   plugins: [react()],
   server: {
     // host: '192.168.1.112'
