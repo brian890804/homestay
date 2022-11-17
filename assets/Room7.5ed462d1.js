83810c458919ef5da1ffa2b69a2552878ca2ef9b
brian890804 <71435901+brian890804@users.noreply.github.com>
2022-11-17 16:15:36 +0800

    Updates

---
+++ assets/Room7.5ed462d1.js
@@ -0,0 +1,1 @@
+const o="https://brian890804.github.io/homestay/assets/Room7.6eb387fc.png";export{o as R};
---
+++ assets/Title.e6aa2a5d.js
@@ -0,0 +1,1 @@
+import{u as s}from"./Routes.be0958eb.js";import{a as o}from"./index.2f43d5da.js";function a({children:t,id:i}){const{isMobile:e}=s();return o("div",{id:i,className:"g-bc g-center text-white fw-bold ",style:{height:e?40:70,fontSize:e?"1rem":"2rem"},children:t})}export{a as T};
