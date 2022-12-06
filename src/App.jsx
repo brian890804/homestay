d53b24c44e4c01b4396049db9122a69619ad4b50
wein <x0930849831@yahoo.com.tw>
2022-12-06 13:33:57 +0800

    feat: add loading animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import { Suspense, lazy, useState } from 'react'
-import JudeEnterWindow from './_basic/other/JudeEnterWindow'
-import JudeWheel from './app/module/Event/JudeWheel'
-import './_basic/css/global.css'
-import './index.css'
-import './app/module/components/css/global.scss'
-const Routes = lazy(() => import('./app/module/pages/Routes'));
+import { Suspense, lazy, useState } from "react";
+import JudeEnterWindow from "./_basic/other/JudeEnterWindow";
+import JudeWheel from "./app/module/Event/JudeWheel";
+import { HashLoader } from "react-spinners";
+import "./_basic/css/global.css";
+import "./index.css";
+import "./app/module/components/css/global.scss";
+const Routes = lazy(() => import("./app/module/pages/Routes"));
 function App() {
   const [show, set] = useState(false);
   JudeEnterWindow();
@@ -13,18 +14,35 @@ function App() {
   return (
     <div
       style={{
-        backgroundColor: '#fff',
-        userSelect: 'none',
-        minHeight: '100vh',
-        height: '100%',
-        overflowY: 'auto',
-        overflowX: 'hidden'
-      }}>
-      <Suspense fallback={"等等"} >
+        backgroundColor: "#fff",
+        userSelect: "none",
+        minHeight: "100vh",
+        height: "100%",
+        overflowY: "auto",
+        overflowX: "hidden",
+        width: "100%",
+      }}
+    >
+      <Suspense
+        fallback={
+          <div
+            style={{
+              width: "100%",
+              height: "100vh",
+              display: "flex",
+              alignItems: "center",
+              placeContent: "center",
+              backgroundColor: "RGB(255, 255, 255, 0.87);",
+            }}
+          >
+            <HashLoader color="#986795" size={50} />
+          </div>
+        }
+      >
         <Routes show={show} />
       </Suspense>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
