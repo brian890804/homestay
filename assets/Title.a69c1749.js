30302eadc7e0b274ab1f56dc9fec1751ee07995f
wein <x0930849831@yahoo.com.tw>
2022-12-06 13:09:33 +0800

    Updates

---
+++ assets/Title.a69c1749.js
@@ -0,0 +1,1 @@
+import{u as s}from"./Routes.e070f685.js";import{a as o}from"./index.abeeaa2a.js";function a({children:t,id:i}){const{isMobile:e}=s();return o("div",{id:i,className:"g-bc g-center text-white fw-bold ",style:{height:e?40:70,fontSize:e?"1rem":"2rem"},children:t})}export{a as T};
