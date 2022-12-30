4853978c1de2e88d5b8acc48a2fdb4c215a7e5ce
wein <71435901+brian890804@users.noreply.github.com>
2022-12-30 15:20:08 +0800

    Updates

---
+++ assets/ContactUs.6ddbffdc.js
@@ -0,0 +1,11 @@
+import{u as t,G as e,s}from"./Routes.3bbee911.js";import{T as l}from"./Title.3701e08a.js";import{j as i,a as u}from"./index.631520d1.js";const r="https://brian890804.github.io/homestay/assets/ContactLine.a3a096b9.png";function m(){const{isMobile:n}=t();return i(o,{isMobile:n,children:[u(l,{id:"contactUs",children:"\u806F\u7D61\u6211\u5011"}),i(e,{container:!0,direction:"row",justifyContent:"center",alignItems:"center",className:!n&&"py-2 px-5",children:[u(e,{item:!0,xs:12,sm:6,children:i("ol",{children:[u("li",{children:"email:legannon24@gmail.com"}),u("li",{children:"LINE\u5B98\u65B9\u5E33\u865F ID:@958fdsep"}),u("li",{children:"WeChat ID:legannon "}),u("li",{children:"\u96FB\u8A71:0986310045 "}),u("li",{children:"\u5730\u5740:\u65B0\u5317\u52E2\u745E\u82B3\u5340\u4E5D\u4EFD\u70CF\u52E2\u6E2F6\u865F"}),u("li",{children:"\u5317\u7E23\u89C0\u5149\u5C40\u6279\u51C6\u5408\u6CD5\u6C11\u5BBF\u7DE8\u865F120\u865F"}),u("li",{children:"99\u5E741\u670812\u65E5\u5317\u5E9C\u89C0\u9928\u5B57\u7B2C0990879564\u865F"}),u("li",{children:"\u7248\u6B0A\u6240\u6709\u70BA\u751F\u6D3B\u7A7A\u9592\u6C11\u5BBF\u3002"})]})}),u(e,{item:!0,xs:12,sm:6,children:u("img",{src:r,alt:"Line \u5C0E\u89BD",style:{width:"100%"}})})]})]})}const o=s.div`
+  /*  */
+  ol {
+    list-style: none;
+    font-size: 1.2rem;
+  }
+  li {
+    font-size: 1.1rem;
+    padding-bottom:0.2em;
+  }
+`;export{m as default};
