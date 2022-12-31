1ac7ae04d57f73ef2c1fa44182fcfc274c8d5c5e
brian890804 <71435901+brian890804@users.noreply.github.com>
2022-12-31 18:25:50 +0800

    Updates

---
+++ assets/LegalHomeStay.770393d7.js
@@ -0,0 +1,36 @@
+import{T as o}from"./Title.1880abad.js";import{u as c,s as n}from"./Routes.e50c7522.js";import{j as e,a as u}from"./index.3a8323c2.js";const l="https://brian890804.github.io/homestay/assets/license.2e5bccdd.png",d="https://brian890804.github.io/homestay/assets/view.ab7da0b8.png",r="https://brian890804.github.io/homestay/assets/1.71575488.png",m="https://brian890804.github.io/homestay/assets/2.830ba095.png",h="https://brian890804.github.io/homestay/assets/3.4a9731ac.png";function p(){const i=[r,m,h],{isMobile:t}=c();return e(g,{children:[u(o,{id:"legalHomeStay",children:"\u65B0\u5317\u5E02\u5408\u6CD5\u6C11\u5BBF"}),e("div",{className:"row body justify-content-center",children:[u("div",{className:"col-lg-4 col-6 mb-3 ",children:u("img",{src:l,className:"body_introduce_img_license",alt:"logo"})}),e("div",{className:`
+                    col-lg-8
+                    col-12 
+                    text-start 
+                    fw-bold fs-6
+                    `,children:[u("div",{className:"row",children:e("div",{className:"col-12 body_introduce_text",children:["\u7248\u6B0A\u6240\u6709\u70BA\u751F\u6D3B\u7A7A\u9593\u6C11\u5BBF\u3002\u96FB\u8A71\uFF1A0986310045 \u5730\u5740\uFF1A \u65B0\u5317\u5E02\u745E\u82B3\u5340\u4E5D\u4EFD\u70CF\u52E2\u5DF76\u865F ",!t&&u("br",{}),"\u5317\u7E23 \u89C0\u5149\u5C40\u6838\u51C6\u5408\u6CD5\u6C11\u5BBF\u7DE8\u865F120\u865F 99\u5E741\u670812\u65E5\u5317\u5E9C\u89C0\u7BA1\u5B57\u7B2C0990879564\u865F"]})}),!t&&u("div",{className:"row mt-2 ",children:i.map((a,s)=>u("div",{className:"col-4 ",children:u("img",{loading:"lazy",src:a,alt:`2022 \u53F0\u7063\u4E5D\u4EFD\u6C11\u5BBF\u63A8\u85A6\u623F\u578B${s}`,className:"body_introduce_img_item"})},s))})]}),u("div",{className:"col-12",children:u("img",{src:d,className:"body_introduce",alt:"\u4E5D\u4EFD\u751F\u6D3B\u7A7A\u9593\u6C11\u5BBF\u89C0\u666F\u53F0"})})]})]})}const g=n.div`
+  /*  */
+  width: 100%;
+  height: 100%;
+  .body {
+    padding: 2%;
+    &_introduce {
+      background-repeat: no-repeat;
+      width: 100%;
+      height: auto;
+      &_img {
+        &_license {
+          object-fit: cover;
+          width: 90%;
+        }
+
+        &_item {
+          width: 100%;
+          height: auto;
+        }
+      }
+      &_text {
+        line-height: 30px;
+        font-size: 1rem;
+        @media (max-width: 599px) {
+          font-size: 0.8rem;
+        }
+      }
+    }
+  }
+`;export{p as default};
