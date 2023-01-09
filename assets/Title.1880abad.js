9fa9014aa648b127d65d4108514db0f5eba06004
brian890804 <71435901+brian890804@users.noreply.github.com>
2023-01-09 22:05:36 +0800

    Updates

--- assets/Title.1880abad.js
+++
@@ -1,1 +0,0 @@
-import{u as s}from"./Routes.e50c7522.js";import{a as o}from"./index.3a8323c2.js";function a({children:t,id:i}){const{isMobile:e}=s();return o("div",{id:i,className:"g-bc g-center text-white fw-bold ",style:{height:e?40:70,fontSize:e?"1rem":"2rem"},children:t})}export{a as T};
---
+++ assets/Title.4c455992.js
@@ -0,0 +1,1 @@
+import{u as s}from"./Routes.bb2ea101.js";import{j as o}from"./index.2a02dfa6.js";function c({children:t,id:i}){const{isMobile:e}=s();return o("div",{id:i,className:"g-bc g-center text-white fw-bold ",style:{height:e?40:70,fontSize:e?"1rem":"2rem"},children:t})}export{c as T};
