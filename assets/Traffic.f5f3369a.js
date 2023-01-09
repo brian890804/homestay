da4dd6ddf1d762282af52c7f96b06712defe5dd1
brian890804 <71435901+brian890804@users.noreply.github.com>
2023-01-09 23:00:10 +0800

    Updates

---
+++ assets/Traffic.f5f3369a.js
@@ -0,0 +1,17 @@
+import{T as a}from"./Title.7164987b.js";import{s as t}from"./Routes.ea8a5abd.js";import{j as e,a as i}from"./index.be2fb50f.js";const r="https://brian890804.github.io/homestay/assets/map.ab1a8b14.png",s="https://brian890804.github.io/homestay/assets/position.93ec8350.png";function l(){return e(m,{children:i("section",{children:[e("header",{children:e("h2",{children:e(a,{id:"traffic",children:"\u4EA4\u901A\u5C0E\u5F15"})})}),i("div",{className:"topArea",children:[e("div",{className:"mr-2",children:e("img",{src:r,alt:"\u4E5D\u4EFD\u751F\u6D3B\u7A7A\u9593\u6C11\u5BBF \u4EA4\u901A\u6307\u5F15\u5716",className:"img"})}),e("div",{className:"ml-2",children:e("img",{src:s,alt:"GoogleMap \u4E5D\u4EFD\u751F\u6D3B\u7A7A\u9593\u6C11\u5BBF",className:"img"})})]}),e("div",{className:"bottomArea",children:e("div",{children:e("iframe",{src:"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1806.3980032916104!2d121.84475700104075!3d25.108765798223853!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x345d451811463bbf%3A0x5f2219e9cb9e39d6!2z55Sf5rS756m66ZaT5rCR5a6_!5e0!3m2!1szh-TW!2stw!4v1673019947388!5m2!1szh-TW!2stw",width:"100%",height:450,style:{border:0},allowFullScreen:!0,loading:"lazy",referrerPolicy:"no-referrer-when-downgrade"})})})]})})}const m=t.div`
+  /*  */
+  transition-duration: 20s;
+  .topArea {
+    padding: 0% 5%;
+    display: flex;
+    margin-bottom: 20px;
+    justify-content: center;
+  }
+  .bottomArea {
+    padding: 0% 5%;
+    margin-bottom: 20px;
+  }
+  .img {
+    width: 100%;
+  }
+`;export{l as default};
